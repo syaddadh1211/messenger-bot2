ddb09e127cbaf9abb90a148b810ceed608218e95
syaddadh1211 <bahalwans@gmail.com>
2021-12-09 17:41:32 +0700

    update env

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,14 @@
 import express from "express";
 import homepageController from "../controllers/homepageController";
+import chatBotController from "../controllers/chatBotController";
+import { route } from "express/lib/application";
+
 let router = express.Router();
 
 let initWebRoutes = (app) => {
   router.get("/", homepageController.getHomepage);
+  router.get("/webhook", chatBotController.getWebHook);
+  router.post("/webhook", chatBotController.postWebHook);
   return app.use("/", router);
 };
 
